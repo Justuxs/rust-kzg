51afe14f23168ab5c3f85e949fa25468ecdf231f
Justuxs <pegasas@yahoo.com>
2023-11-19 16:35:50 +0200

    remove feature

--- blst/src/types/multiscalar_mul.rs
+++ blst/src/types/multiscalar_mul.rs
@@ -1,43 +1,15 @@
+
 //! Multiscalar multiplication implementation using pippenger algorithm.
-// use dusk_bytes::Serializable;
-
-// use alloc::vec::*;
-
-
+use blst::{blst_fr, blst_p1, blst_p1_add, blst_p1_double, blst_p1_in_g1};
 use kzg::{Fr, G1};
 use crate::types::fr::{FsFr as Scalar, FsFr};
-use crate::types::g1::FsG1;
+use crate::types::g1::{FsG1 as G1Projective, FsG1};
+/// Performs multiscalar multiplication reliying on Pippenger's algorithm.
+/// This method was taken from `curve25519-dalek` and was originally made by
+/// Oleg Andreev <oleganza@gmail.com>.
 
-pub fn divn(mut scalar: Scalar, mut n: u32) -> Scalar {
-    if n >= 256 {
-        return Scalar::zero()
-    }
-
-    while n >= 64 {
-        let mut t = 0;
-        for i in scalar.0.iter_mut().rev() {
-            core::mem::swap(&mut t, i);
-        }
-        n -= 64;
-    }
-
-    if n > 0 {
-        let mut t = 0;
-        for i in scalar.0iter_mut().rev() {
-            let t2 = *i << (64 - n);
-            *i >>= n;
-            *i |= t;
-            t = t2;
-        }
-    }
-
-    scalar
-}
-
-/// Performs a Variable Base Multiscalar Multiplication.
-
-pub fn msm_variable_base(points: &[FsG1], scalars: &[FsFr]) -> FsG1 {
-
+#[allow(clippy::needless_collect)]
+pub fn msm_variable_base(points: &[G1Projective], scalars: &[Scalar]) -> G1Projective {
     #[cfg(feature = "parallel")]
     use rayon::prelude::*;
 
@@ -50,13 +22,13 @@ pub fn msm_variable_base(points: &[FsG1], scalars: &[FsFr]) -> FsG1 {
     let num_bits = 255usize;
     let fr_one = Scalar::one();
 
-    let zero = FsG1::identity();
+    let zero = G1Projective::identity();
     let window_starts: Vec<_> = (0..num_bits).step_by(c).collect();
 
     #[cfg(feature = "parallel")]
-    let window_starts_iter = window_starts.into_par_iter();
+        let window_starts_iter = window_starts.into_par_iter();
     #[cfg(not(feature = "parallel"))]
-    let window_starts_iter = window_starts.into_iter();
+        let window_starts_iter = window_starts.into_iter();
 
     // Each window is of size `c`.
     // We divide up the bits 0..num_bits into windows of size `c`, and
@@ -77,37 +49,46 @@ pub fn msm_variable_base(points: &[FsG1], scalars: &[FsFr]) -> FsG1 {
                             res = res.add(base);
                         }
                     } else {
-                        let mut scalar = Scalar::montgomery_reduce(
-                            scalar.0[0],
-                            scalar.0[1],
-                            scalar.0[2],
-                            scalar.0[3],
+
+                        // We right-shift by w_start, thus getting rid of the
+                        // lower bits.
+                        let mut scalar = montgomery_reduce(
+                            scalar.0.l[0],
+                            scalar.0.l[1],
+                            scalar.0.l[2],
+                            scalar.0.l[3],
                             0,
                             0,
                             0,
                             0,
                         );
 
-                        // We right-shift by w_start, thus getting rid of the
-                        // lower bits.
-                        scalar = divn(scalar, w_start as u32);
+                        let scalar = divn(scalar, w_start as u32);
+
                         // We mod the remaining bits by the window size.
-                        let scalar = scalar.0[0] % (1 << c);
+                        let inside = scalar.0;
+                        let scalar = inside.l[0] % (1 << c);
 
                         // If the scalar is non-zero, we update the corresponding
                         // bucket.
                         // (Recall that `buckets` doesn't have a zero bucket.)
                         if scalar != 0 {
+                            let space = (scalar - 1) as usize;
                             buckets[(scalar - 1) as usize] =
                                 buckets[(scalar - 1) as usize].add(base);
                         }
                     }
                 });
-
-            let mut running_sum = FsG1::identity();
-            for b in buckets.into_iter().rev() {
-                running_sum += b;
-                res += &running_sum;
+            let mut i = 0;
+            let mut running_sum = G1Projective::identity();
+            for b in buckets.into_iter(){
+                i = i+1;
+                let respo = running_sum.add(&b);
+                let res = res.add(&respo);
+                if( i == 429){
+                    let running_sume = running_sum;
+                    let rese = res;
+                }
             }
 
             res
@@ -117,17 +98,26 @@ pub fn msm_variable_base(points: &[FsG1], scalars: &[FsFr]) -> FsG1 {
     // We store the sum for the lowest window.
     let lowest = *window_sums.first().unwrap();
     // We're traversing windows from high to low.
-    window_sums[1..]
+    let other = window_sums[1..]
         .iter()
         .rev()
-        .fold(zero, |mut total, sum_i| {
-            total += sum_i;
+        .fold(zero, |mut total, sum_i| unsafe {
+            total = total.add(sum_i);
             for _ in 0..c {
-                total = total.double();
+                let mut total_p1 = blst_p1::default();
+                let raw_pointer = &mut total_p1 as *mut blst_p1;
+                let total_p1_ptr = &mut total.0 as *mut blst_p1;
+
+               blst_p1_double(raw_pointer, total_p1_ptr);
+                total.0 = *raw_pointer;
+                let ok = total.0;
+                let ww= *raw_pointer;
+                let ss = *total_p1_ptr;
             }
             total
-        })
-        + lowest
+        });
+    let answer = other.add(&lowest);
+    return answer;
 }
 
 fn ln_without_floats(a: usize) -> usize {
@@ -143,49 +133,108 @@ fn log2(x: usize) -> u32 {
     core::mem::size_of::<usize>() as u32 * 8 - n
 }
 
-/*
+#[inline]
+pub fn divn(mut scalar: Scalar, mut n: u32) -> Scalar {
+    if n >= 256 {
+        return Scalar::zero();
+    }
 
-mod tests {
-    #[allow(unused_imports)]
-    use super::*;
+    while n >= 64 {
+        let mut t = 0;
+        for i in scalar.0.l.iter_mut().rev() {
+            core::mem::swap(&mut t, i);
+        }
+        n -= 64;
+    }
 
-    #[test]
-    fn pippenger_test() {
-        // Reuse points across different tests
-        let mut n = 512;
-        let x = Scalar::from(2128506u64).invert().unwrap();
-        let y = Scalar::from(4443282u64).invert().unwrap();
-        let points = (0..n)
-            .map(|i| G1Projective::generator() * Scalar::from(1 + i as u64))
-            .collect::<Vec<_>>();
-        let scalars = (0..n)
-            .map(|i| x + (Scalar::from(i as u64) * y))
-            .collect::<Vec<_>>(); // fast way to make ~random but deterministic scalars
-        let premultiplied: Vec<G1Projective> = scalars
-            .iter()
-            .zip(points.iter())
-            .map(|(sc, pt)| pt * sc)
-            .collect();
-        while n > 0 {
-            let scalars = &scalars[0..n];
-            let points = &points[0..n];
-            let control: G1Projective = premultiplied[0..n].iter().sum();
-            let subject = pippenger(
-                points.to_owned().into_iter(),
-                scalars.to_owned().into_iter(),
-            );
-            assert_eq!(subject, control);
-            n = n / 2;
+    if n > 0 {
+        let mut t = 0;
+        for i in scalar.0.l.iter_mut().rev() {
+            let t2 = *i << (64 - n);
+            *i >>= n;
+            *i |= t;
+            t = t2;
         }
     }
 
-    #[test]
-    fn msm_variable_base_test() {
-        let points = vec![G1Affine::generator()];
-        let scalars = vec![Scalar::from(100u64)];
-        let premultiplied = G1Projective::generator() * Scalar::from(100u64);
-        let subject = msm_variable_base(&points, &scalars);
-        assert_eq!(subject, premultiplied);
-    }
+    scalar
 }
-*/
+
+pub const MODULUS: Scalar = Scalar(blst_fr{
+    l: [
+        0xffff_ffff_0000_0001,
+        0x53bd_a402_fffe_5bfe,
+        0x3339_d808_09a1_d805,
+        0x73ed_a753_299d_7d48,
+    ],
+});
+const INV: u64 = 0xffff_fffe_ffff_ffff;
+
+#[inline(always)]
+pub const fn mac(a: u64, b: u64, c: u64, carry: u64) -> (u64, u64) {
+    let ret = (a as u128) + ((b as u128) * (c as u128)) + (carry as u128);
+    (ret as u64, (ret >> 64) as u64)
+}
+#[inline(always)]
+pub const fn adc(a: u64, b: u64, carry: u64) -> (u64, u64) {
+    let ret = (a as u128) + (b as u128) + (carry as u128);
+    (ret as u64, (ret >> 64) as u64)
+}
+#[inline(always)]
+pub fn montgomery_reduce(
+    r0: u64,
+    r1: u64,
+    r2: u64,
+    r3: u64,
+    r4: u64,
+    r5: u64,
+    r6: u64,
+    r7: u64,
+) -> Scalar {
+    // The Montgomery reduction here is based on Algorithm 14.32 in
+    // Handbook of Applied Cryptography
+    // <http://cacr.uwaterloo.ca/hac/about/chap14.pdf>.
+
+    let k = r0.wrapping_mul(INV);
+    let (_, carry) = mac(r0, k, MODULUS.0.l[0], 0);
+    let (r1, carry) = mac(r1, k, MODULUS.0.l[1], carry);
+    let (r2, carry) = mac(r2, k, MODULUS.0.l[2], carry);
+    let (r3, carry) = mac(r3, k, MODULUS.0.l[3], carry);
+    let (r4, carry2) = adc(r4, 0, carry);
+
+    let k = r1.wrapping_mul(INV);
+    let (_, carry) = mac(r1, k, MODULUS.0.l[0], 0);
+    let (r2, carry) = mac(r2, k, MODULUS.0.l[1], carry);
+    let (r3, carry) = mac(r3, k, MODULUS.0.l[2], carry);
+    let (r4, carry) = mac(r4, k, MODULUS.0.l[3], carry);
+    let (r5, carry2) = adc(r5, carry2, carry);
+
+    let k = r2.wrapping_mul(INV);
+    let (_, carry) = mac(r2, k, MODULUS.0.l[0], 0);
+    let (r3, carry) = mac(r3, k, MODULUS.0.l[1], carry);
+    let (r4, carry) = mac(r4, k, MODULUS.0.l[2], carry);
+    let (r5, carry) = mac(r5, k, MODULUS.0.l[3], carry);
+    let (r6, carry2) = adc(r6, carry2, carry);
+
+    let k = r3.wrapping_mul(INV);
+    let (_, carry) = mac(r3, k, MODULUS.0.l[0], 0);
+    let (r4, carry) = mac(r4, k, MODULUS.0.l[1], carry);
+    let (r5, carry) = mac(r5, k, MODULUS.0.l[2], carry);
+    let (r6, carry) = mac(r6, k, MODULUS.0.l[3], carry);
+    let (r7, _) = adc(r7, carry2, carry);
+
+    let blst_fr = blst_fr{
+        l: [
+            r4,
+            r5,
+            r6,
+            r7,
+        ],
+    };
+
+    let mut scalar = Scalar(blst_fr);
+    let modulus = MODULUS;
+    scalar.sub(&modulus);
+    scalar
+}
+
